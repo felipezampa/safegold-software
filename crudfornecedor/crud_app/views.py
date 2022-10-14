cd7cb5e88a0f01fdc50f80c64b7712db718d9459
Felipe <felipeavzampa90@gmail.com>
2022-10-14 15:12:48 -0300

    CRUD Pronto version 1.0

--- crudfornecedor/crud_app/views.py
+++ crudfornecedor/crud_app/views.py
@@ -5,13 +5,12 @@ from django.views.generic import (TemplateView,ListView,CreateView,DeleteView,Up
 from . import models
 
 
+
 class IndexView(TemplateView):
      template_name = 'crud_app/index.html'
 
 class ContaListView(ListView):
     template_name = 'crud_app/tabela.html'
-    # Example of making your own:
-    # context_object_name = 'schools'
     model = models.Conta
 
 class ContaCreateView(CreateView):
@@ -21,23 +20,16 @@ class ContaCreateView(CreateView):
     success_url = reverse_lazy("crud_app:list")
 
 class ContaDeleteView(DeleteView):
-    model = models.Conta
-    template_name= 'crud_app/excluir-cadastro.html'
-    success_url = reverse_lazy("crud_app:list")
-
-
-
-
-
-
-
+     model = models.Conta
+     template_name= 'crud_app/excluir-cadastro.html'
+     success_url = reverse_lazy("crud_app:list")
 
 class ContaUpdateView(UpdateView):
+    template_name = 'crud_app/cadastro.html'
     fields = ("desc","fornecedor")
     model = models.Conta
-
+    success_url = reverse_lazy("crud_app:list")
 
 class ContaDetailView(DetailView):
-    context_object_name = 'fornecedor_details'
-    model = models.Fornecedor
-    template_name = 'crud_app/school_detail.html'
+    model = models.Conta 
+    template_name = 'crud_app/detail.html'
