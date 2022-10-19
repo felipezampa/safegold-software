338413b6979bcd8fb1a60466cc4e4ddf6d9f9b99
Felipe <felipeavzampa90@gmail.com>
2022-10-19 17:34:49 -0300

    Its wednesday my dudes

--- crudfornecedor/static/js/tabela.js
+++ crudfornecedor/static/js/tabela.js
@@ -7,7 +7,32 @@ $(document).ready(
 $(document).ready(
     function exportPrint() {
         $(document).on("click", "#printExport", function () {
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+            var min = today.getMinutes();
+            var hour = today.getHours();
+
+            today = 'Relatório emitido em:>= ' + dd + '/' + mm + '/' + yyyy + ' às: ' + hour + ':' + min;
+            // Create an "li" node:
+            const node = document.createElement("h5");
+            // Create a text node:
+            const textnode = document.createTextNode(today);
+
+            // Append the text node to the "li" node:
+            node.appendChild(textnode);
+
+            // Append the "li" node to the list:
+            document.getElementById("print-info").appendChild(node);
             window.print();
+            document.getElementById("print-info").removeChild(node);
         });
     }
 );
+
+
+function DarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
