e3f367e0864d3aa99f12dc16c9da570cbb2e62ee
Felipe <felipeavzampa90@gmail.com>
2022-11-04 09:26:53 -0300

    apagado modelos antigos de conta e fornecedor

--- crud_app/admin.py
+++ crud_app/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Conta)
-admin.site.register(Fornecedor)
-
-
 admin.site.register(Bairros)
 admin.site.register(Cidades)
 admin.site.register(Dimcontas)
