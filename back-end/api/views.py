9dd5610b399a901f773e8a018bb1b12ddf65f917
Eduardo Levi Cordeiro <93154356+Duduelc7@users.noreply.github.com>
2022-12-22 15:29:33 -0300

    tirei a api da função generica do DRF

--- back-end/api/views.py
+++ back-end/api/views.py
@@ -5,13 +5,55 @@ from crud_app import models
 from api import serializers
 
 from django.http import JsonResponse
+
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
 # Create your views here.
 
 
+@csrf_exempt
 def empresa_list_api(request):
-    empresa = models.Empresas.objects.all()
-    serializer = serializers.EmpresasSerializer(empresa, many=True)
-    return JsonResponse({'Empresas': serializer.data},safe=False)
+    if request.method == 'GET':
+        empresa = models.Empresas.objects.all()
+        serializer = serializers.EmpresasSerializer(empresa, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = serializers.EmpresasSerializer(data= data)
+        if serializer.is_valid():
+            serializer.save()
+            JsonResponse(serializer.data, status=201)
+        JsonResponse(serializer.errors, status=400)
+        
+@csrf_exempt
+def empresa_detail_api(request, pk):
+    try:
+        empresa = models.Empresas.objects.get(cod_empresa=pk)
+    except models.Empresas.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = serializers.EmpresasSerializer(empresa)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser.parse(request)
+        serializer = serializers.EmpresasSerializer(empresa, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        empresa.delete()
+        return HttpResponse(status = 204)
+
+    
+
+
+
 
 
 def projetos_list_api(request):
@@ -20,10 +62,10 @@ def projetos_list_api(request):
     return JsonResponse({'Projetos': serializer.data}, safe=False)
 
 
-def users_list_api(request):
-    user = models.User.objects.all()
-    serializer = serializers.UserSerializer(user, many=True)
-    return JsonResponse({'Users': serializer.data}, safe=False)
+# def users_list_api(request):
+#     user = models.User.objects.all()
+#     serializer = serializers.UserSerializer(user, many=True)
+#     return JsonResponse({'Users': serializer.data}, safe=False)
 
 def fin_grupocontas_api(request):
     pass
@@ -32,3 +74,46 @@ def fin_subgrupocontas_api(request):
     pass
 def fin_planocontas_api(request):
     pass
+
+
+@csrf_exempt
+def user_list_api(request):
+    if request.method == 'GET':
+        user = models.User.objects.all()
+        serializer = serializers.UserSerializer(user, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = serializers.UserSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def user_detail_api(request,pk):
+    try:
+        user = models.User.objects.get(pk = pk)
+        
+    except models.User.DoesNotExist:
+        return HttpResponse(status=400)
+
+    if request.method =='GET':
+        serializer = serializers.UserSerializer(user)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = serializers.UserSerializer(user, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return HttpResponse(status=204)
+
+
+
+
