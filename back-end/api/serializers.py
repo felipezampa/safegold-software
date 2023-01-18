b26421b5d677fb1a4ee190204f1bb2e31cd2b8e3
Eduardo Levi Cordeiro <93154356+Duduelc7@users.noreply.github.com>
2023-01-18 15:41:31 -0300

    documentação nas api's

--- back-end/api/serializers.py
+++ back-end/api/serializers.py
@@ -1,6 +1,23 @@
 from rest_framework import serializers
 from crud_app.models import  *
+'''
+    Implementaçã do ModelSerializer
+        faz a serialização da tabela no models.py
 
+        - é nela que voce pode fazer api's aninhadas, como foi necessario nesse projeto
+            como é o caso do ProjetosSerializers onde recebe a api Users como aninhada para criação de contexto
+
+        
+        model = Classe do models.py que deseja serializar para DRF
+
+        fields = recebe uma tupla com os campos que deseja exibir na APi
+            * voce pode passar como '__all__' para pegar todos os campos da tabela
+
+
+        
+
+
+'''
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- back-end/api/views.py
+++ back-end/api/views.py
@@ -4,6 +4,16 @@ from api import serializers
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
+
+'''
+    Construção do ViewSet da api
+        - queryset = Faz a busca no banco de dados, semelhante a um 'SELECT * FROM xtable'
+        - serializer_class = recebe o serializes do serializer.py
+        - filter_backends = Implementação de filtros do DRF
+        - filterset_fields = Recebe uma lista dos campos que serão filtrados
+        - search_fields = Recebe uma lista dos campos que serão filtrados no campo de search
+
+'''
 class EmpresaserializerViewSet(viewsets.ModelViewSet):
     queryset = models.Empresas.objects.all()
     serializer_class = serializers.EmpresasSerializer
@@ -37,4 +47,25 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['username', 'email', 'id']
+
+
+
+
+
+'''
+
+    Autenticação JWT (JSON Web Token)
+        - JWT é usado para criar tokens de acesso a um app
+        - O servidor gera um token que certifica a identidade do usuário e o envia ao cliente
+
+        Instale o pacote do django-rest-framework-simplejwt:
+
+          '  pip instalar djangorestframework-simplejwt  '
+
+        
+
+
+    referencia = https://medium.com/django-rest/django-rest-framework-jwt-authentication-94bee36f2af8
+
+'''
 
