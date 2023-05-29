c36024eea07b8e0acf5e65154dd2a6b8fa4eea2b
Felipe Zampa <86922370+felipezampa@users.noreply.github.com>
2023-05-29 15:23:14 -0300

    validacao erros no filtro de data historico agenda

--- front-end/src/app/agenda/agenda-historico/agenda-historico.component.ts
+++ front-end/src/app/agenda/agenda-historico/agenda-historico.component.ts
@@ -26,29 +26,6 @@ export class AgendaHistoricoComponent implements OnInit {
     this.verSemanaAtual();
   }
 
-  filtrarPorData(ini: Date, fim: Date) {
-    // Limpa a flag dos botões selecionados
-    this.semanaSelecionada = '';
-    try {
-      if (ini > fim) {
-        throw new Error('Data inicial não pode ser maior que a data final');
-      } else if (ini == fim) {
-        throw new Error('As datas de inicio e fim não podem ser iguais');
-      } else {
-        // Atribui os valores dos inputs nos atributos locais
-        this.diaFim = fim;
-        this.diaInicio = ini;
-      }
-    } catch (e) {
-      //Mostra a exceção na tela
-      this.mensagemErro = '<h4 class="alert alert-danger strong">' + e + '</h4>';
-    } finally {
-      // Faz a chamada do metodo de filtro personalizado
-      this.filtrarAgenda(ini, fim);
-    }
-
-  }
-
   filtrarAgenda(inicio: Date | String, fim: Date | String) {
     // Lista todos os dados da agenda
     this.agendaService.listAgendaHistorico().subscribe(filtro => {
@@ -63,9 +40,35 @@ export class AgendaHistoricoComponent implements OnInit {
     this.authService.logout();
   }
 
+  filtrarPorData(ini: Date, fim: Date) {
+    // Limpa a flag dos botões selecionados
+    this.semanaSelecionada = '';
+    try {
+      if (ini > fim) {
+        throw new Error('Data inicial não pode ser maior que a data final');
+      } else if (ini == fim) {
+        throw new Error('As datas de inicio e fim não podem ser iguais');
+      } else {
+        this.mensagemErro = '';
+        // Atribui os valores dos inputs nos atributos locais
+        this.diaFim = fim;
+        this.diaInicio = ini;
+      }
+    } catch (e) {
+      //Mostra a exceção na tela
+      this.mensagemErro = '<h4 class="alert alert-danger strong">' + e + '</h4>';
+    } finally {
+      // Faz a chamada do metodo de filtro personalizado
+      this.filtrarAgenda(ini, fim);
+    }
+
+  }
+
   verSemanaAtual() {
     // Flag do Botao
     this.semanaSelecionada = 'atual';
+    // Flag caso tenha erros
+    this.mensagemErro = '';
     // pega data atual
     var curr = new Date;
     // Primeiro eh o dia do mes - o dia da semana
@@ -81,26 +84,18 @@ export class AgendaHistoricoComponent implements OnInit {
     this.diaInicio = new Date(firstday);
     this.diaFim = new Date(lastday);
 
-    console.log(firstday);
-    console.log(lastday);
-    console.log('---------------------');
-    // const date = new Date(); // Use sua própria variável 'date' ou 'this.diaFim'
-
-    // const year = date.getFullYear();
-    // const month = String(date.getMonth() + 1).padStart(2, '0'); // Adiciona o zero à esquerda se necessário
-    // const day = String(date .getDate()).padStart(2, '0'); // Adiciona o zero à esquerda se necessário
-
     const format = 'yyyy-MM-dd';
     const locale = 'en-US';
     const ini = formatDate(new Date(firstday), format, locale);
     const fim = formatDate(new Date(lastday), format, locale);
-    // console.log(formattedDate); // Output: "2023-05-26"
-
     this.filtrarAgenda(ini, fim);
   }
+
   verSemanaPassada() {
     // Flag do Botao
     this.semanaSelecionada = 'passada';
+    // Flag caso tenha erros
+    this.mensagemErro = '';
     // Prepara as datas da semana que vem
     var curr = new Date; // pega data atual
     var first = curr.getDate() - curr.getDay() - 7; // Primeiro eh o dia do mes - o dia da semana
@@ -111,19 +106,19 @@ export class AgendaHistoricoComponent implements OnInit {
     let lastday = new Date(curr.setDate(last)).toUTCString(); // Variavel de data
     this.diaInicio = new Date(firstday); // Converte para o atributo da classe
     this.diaFim = new Date(lastday); // Converte para o atributo da classe
-    // Loop para alterar as datas dos cards
-    // this.diasSemana.forEach((dia) => {
-    //   let firstdayCard = new Date(curr.setDate(first)).toUTCString(); // Variavel de data
-    //   dia.dia = new Date(firstdayCard); // Atribui o dia no objeto dia para a data
-    //   first++; // Incrementa a data
-    // });
-    // // Remove os cartões da semana que vem e adiciona novos para semana atual
-    // this.criarPrimeiroCard();
-    // this.filtrarAgenda();
+
+    const format = 'yyyy-MM-dd';
+    const locale = 'en-US';
+    const ini = formatDate(new Date(firstday), format, locale);
+    const fim = formatDate(new Date(lastday), format, locale);
+    this.filtrarAgenda(ini, fim);
   }
+
   verProximaSemana() {
     // Flag do Botao
     this.semanaSelecionada = 'proxima';
+    // Flag caso tenha erros
+    this.mensagemErro = '';
     // Prepara as datas da semana que vem
     let curr = new Date; // pega data atual
     let first = curr.getDate() - curr.getDay(); // Primeiro eh o dia do mes - o dia da semana
@@ -134,26 +129,11 @@ export class AgendaHistoricoComponent implements OnInit {
     let lastday = new Date(curr.getFullYear(), curr.getMonth(), last).toUTCString(); // Variavel de data
     this.diaInicio = new Date(firstday); // Converte para o atributo da classe
     this.diaFim = new Date(lastday); // Converte para o atributo da classe
-    // // Loop para alterar as datas dos cards
-    // this.diasSemana.forEach((dia) => {
-    //   let firstdayCard = new Date(curr.setDate(first)).toUTCString(); // Variavel de data
-    //   dia.dia = new Date(firstdayCard); // Atribui o dia no objeto dia para a data
-    //   first++; // Incrementa a data
-    // });
-    // // Remove os cartões da semana atual e adiciona novos para semana que vem
-    // this.criarPrimeiroCard();
-
-    // );
-    // this.matrizService.listMatrizAnalitica()
-    // .subscribe(filtro => {
-    //   this.matrizAnalitica = filtro.filter(
-    //     // Compara filtro com o array tudo em lowercase
-    //     matriz => matriz.desc_fornecedor.toLowerCase().includes(this.filtroNome.toLowerCase())
-    //   );
-    //   // Ordena por nome crescente
-    //   this.matrizAnalitica.sort((a, b) => (a.desc_fornecedor ?? '').localeCompare(b.desc_fornecedor ?? ''))
-    //   this.isLoading = false;
-    // });
+    const format = 'yyyy-MM-dd';
+    const locale = 'en-US';
+    const ini = formatDate(new Date(firstday), format, locale);
+    const fim = formatDate(new Date(lastday), format, locale);
+    this.filtrarAgenda(ini, fim);
   }
   listarAgenda() {
     this.agendaService.listAgendaHistorico().subscribe({
@@ -164,5 +144,12 @@ export class AgendaHistoricoComponent implements OnInit {
       }
     });
 
+  }
+  fazerTabelaListrada(isEven: boolean): object {
+    if (isEven) {
+      return { 'background-color': '#f2f2f2' };
+    } else {
+      return {};
+    }
   }
 }
